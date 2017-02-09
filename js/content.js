82151b7cc5883bcaf8763e87abed486515dee677
quotto <rhyme.and.rhyme@gmail.com>
2017-02-09 23:17:44 +0900

    released

--- js/content.js
+++ js/content.js
@@ -1,23 +1,27 @@
 $(document).ready(function(){
-  var $uploading_modal = $('<div id="uploadModal" class="modal fade" tabindex="-1" role="dialog"></div>');
+  var $upload_modal = $('<div id="upload-modal" role="dialog"></div>');
 
-  var $uploading_modal_document = $('<div class="modal-dialog modal-sm" role="document"></div>');
-  var $uploading_modal_document_content = $('<div class="modal-content"></div>');
-  var $uploading_modal_document_content_body = $('<div class="modal-body"></div>');
-  var $uploading_message = $('<p></p>');
+  var $upload_modal_content = $('<div id="upload-modal-content"></div>');
+  var $upload_message = $('<p></p>');
 
-  $uploading_modal_document_content_body.append($uploading_message);
-  $uploading_modal_document_content.append($uploading_modal_document_content_body);
-  $uploading_modal_document.append($uploading_modal_document_content);
-  $uploading_modal.append($uploading_modal_document);
+  $upload_modal_content.append($upload_message);
+  $upload_modal.append($upload_modal_content);
+  $upload_modal.on('click',function(){
+    $(this).fadeOut();
+  });
+  $upload_modal.on('mousemove',function(){
+    $(this).fadeOut();
+  });
 
-  $('body').append($uploading_modal);
+  $('body').append($upload_modal);
 
   chrome.runtime.onMessage.addListener(function(message) {
+    $(this).blur();
     var json_message = JSON.parse(message);
-    var $uploading_modal = $('#uploadModal');
-    $uploading_modal.find('p').text(json_message['message']);
+    var $upload_modal = $('#upload-modal');
+    var $upload_modal_content = $('#upload-modal-content');
+    $upload_modal_content.find('p').text(json_message['message']);
 
-    $('#uploadModal').modal('show');
+    $('#upload-modal').fadeIn();
   });
 });
