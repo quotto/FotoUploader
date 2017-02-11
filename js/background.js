a46ccca1577ae1998a1fd105f8489ac2a125be72
quotto <rhyme.and.rhyme@gmail.com>
2017-02-11 19:16:56 +0900

    add scale chenge function

--- js/background.js
+++ js/background.js
@@ -1,6 +1,8 @@
 var changeUploadingModal = function(tab_id,status_text,message) {
   "use strict";
-  var send_message = '{"status": "' + status_text + '","message": "' + message + '"}'; 
+  message = message.replace(/\//g,'\\\/');
+  message = message.replace(/"/g,'\\\"');
+  var send_message = '{"type": "notice","status": "' + status_text + '", "message": "' + message + '"}';
   chrome.tabs.sendMessage(tab_id,send_message);
 }
 var getIsoString = function (){
@@ -23,79 +25,95 @@ var getIsoString = function (){
   }
   return year + "-" + month + "-" + date + "T" + hour + ":" + minute + ":" + sec +"." + pad + mil +"Z";
 }
-chrome.contextMenus.create({
-  id: "fotouploader",
-  title: "はてなフォトライフへアップロード",
-  onclick: function(info, tab) {
-    chrome.storage.local.get(['FotolifeUploader_HatenaId','FotolifeUploader_ApiKey','FotolifeUploader_Folder'],function(value){
-      changeUploadingModal(tab.id,'load','はてなフォトライフへアップロードしています……');
-      var xhr = new XMLHttpRequest();
-      var src = info.srcUrl;
-      xhr.open('GET',src);
-      xhr.responseType = 'arraybuffer';
-      xhr.onload = function(e) {
-        var bytes = new Uint8Array(this.response);
-        var descriptor_hex = '';
-        for (var i = 0; i < 8; i++) {
-          descriptor_hex += bytes[i].toString(16);
-        }
 
-        var descriptor = '';
-        if(descriptor_hex.substr(0,4) === 'ffd8') {
-          descriptor = 'jpeg';
-        } else if(descriptor_hex.substr(0,8) === '89504e47') {
-          descriptor = 'png';
-        } else if(descriptor_hex.substr(0,8) === '47494638') {
-          descriptor = 'gif';
-        } else if(descriptor_hex.substr(0,4) === '424d') {
-          descriptor = 'bmp';
-        } else {
-          changeUploadModal(tab.id,'error','この画像はアップロードできません。');
-          return;
-        }
-
-        var binaryData = '';
-        for(var i = 0; i < bytes.byteLength; i++) {
-          binaryData += String.fromCharCode(bytes[i]);
-        }
-
-        var upload_url = 'https://hatenablog.wackwack.net/hatena_blog/fotolife_upload';
-        var hatena_id = value.FotolifeUploader_HatenaId;
-        var api_key = value.FotolifeUploader_ApiKey;
-        var folder_name = value.FotolifeUploader_Folder;
-        var sha_obj = new jsSHA('SHA-1','TEXT');
-        var rand_str= Math.random().toString();
-        sha_obj.update(rand_str);
-        var nonce = sha_obj.getHash('B64');
-        var base64nonce = btoa(nonce);
-
-        var timestamp = getIsoString();
-
-        var sha_obj2 = new jsSHA('SHA-1','TEXT',{numRounds:1});
-        sha_obj2.update(nonce + timestamp + api_key);
-        var digest = sha_obj2.getHash('B64');
-        $.ajax({
-          url: upload_url,
-          method: 'POST',
-          data: {descriptor: descriptor, imagedata: btoa(binaryData), folder: folder_name,id: hatena_id,api_key: api_key, digest: digest, nonce: base64nonce, timestamp: timestamp},
-          dataType: 'json'
-        }).done(function(data,statusText,jqXHR) { 
-          changeUploadingModal(tab.id,'success','アップロードが完了しました。');
-        }).fail(function(jqXHR,statusText,errorThrown) {
-          var message = '';
-          switch(jqXHR.status) {
-            case 403:
-              message = 'フォトライフのアップロードに失敗しました。はてなidとAPIキーの設定を確認して下さい。';
-              break;  
-            default:
-              message = 'エラーが発生しました。しばらく経ってから実行するか、開発者にお問い合わせください。';
-              break;
-          }
-          changeUploadingModal(tab.id,'error',message);
-        });
+var doUpload = function(src,tab,scale) {
+  "use strict";
+  chrome.storage.local.get(['FotolifeUploader_HatenaId','FotolifeUploader_ApiKey','FotolifeUploader_Folder'],function(value){
+    changeUploadingModal(tab.id,'load','はてなフォトライフへアップロードしています……');
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET',src);
+    xhr.responseType = 'arraybuffer';
+    xhr.onload = function(e) {
+      var bytes = new Uint8Array(this.response);
+      var descriptor_hex = '';
+      for (var i = 0; i < 8; i++) {
+        descriptor_hex += bytes[i].toString(16);
       }
-      xhr.send();
-    });
+
+      var descriptor = '';
+      if(descriptor_hex.substr(0,4) === 'ffd8') {
+        descriptor = 'jpeg';
+      } else if(descriptor_hex.substr(0,8) === '89504e47') {
+        descriptor = 'png';
+      } else if(descriptor_hex.substr(0,8) === '47494638') {
+        descriptor = 'gif';
+      } else if(descriptor_hex.substr(0,4) === '424d') {
+        descriptor = 'bmp';
+      } else {
+        changeUploadingModal(tab.id,'error','この画像はアップロードできません。');
+        return;
+      }
+
+      var binaryData = '';
+      for(var i = 0; i < bytes.byteLength; i++) {
+        binaryData += String.fromCharCode(bytes[i]);
+      }
+
+      // var upload_url = 'https://hatenablog.wackwack.net/hatena_blog/fotolife_upload';
+      var upload_url = 'http://localhost:3000/hatena_blog/fotolife_upload';
+      var hatena_id = value.FotolifeUploader_HatenaId;
+      var api_key = value.FotolifeUploader_ApiKey;
+      var folder_name = value.FotolifeUploader_Folder;
+      var sha_obj = new jsSHA('SHA-1','TEXT');
+      var rand_str= Math.random().toString();
+      sha_obj.update(rand_str);
+      var nonce = sha_obj.getHash('B64');
+      var base64nonce = btoa(nonce);
+
+      var timestamp = getIsoString();
+
+      var sha_obj2 = new jsSHA('SHA-1','TEXT',{numRounds:1});
+      sha_obj2.update(nonce + timestamp + api_key);
+      var digest = sha_obj2.getHash('B64');
+      $.ajax({
+        url: upload_url,
+        method: 'POST',
+        data: {descriptor: descriptor, imagedata: btoa(binaryData), folder: folder_name,id: hatena_id,api_key: api_key, digest: digest, nonce: base64nonce, timestamp: timestamp, scale: scale},
+        dataType: 'json'
+      }).done(function(data,statusText,jqXHR) { 
+        changeUploadingModal(tab.id,'success','アップロードが完了しました。');
+      }).fail(function(jqXHR,statusText,errorThrown) {
+        var message = '';
+        switch(jqXHR.status) {
+          case 403:
+            message = 'フォトライフのアップロードに失敗しました。はてなidとAPIキーの設定を確認して下さい。';
+            break;  
+          default:
+            message = 'エラーが発生しました。しばらく経ってから実行するか<a href="https://twitter.com/quo1987">開発者にお問い合わせください。</a>';
+            break;
+        }
+        changeUploadingModal(tab.id,'error',message);
+      });
+    }
+    xhr.send();
+  });
+}
+
+chrome.runtime.onMessage.addListener(function(request,sender,sendResponse) {
+  doUpload(request.src,sender.tab,request.scale);
+});
+
+chrome.contextMenus.create({
+  title: "そのままアップロード",
+  onclick: function(info,tab) {
+    doUpload(info.srcUrl,tab,100);
   },
   contexts:['image']
+});
+chrome.contextMenus.create({
+  title: "画像サイズを変えてアップロード",
+  contexts:['image'],
+  onclick: function(info,tab) {
+    chrome.tabs.sendMessage(tab.id,'{"type": "set_scale", "src": "' + info.srcUrl + '"}');
+  }
 });
--- js/content.js
+++ js/content.js
@@ -1,27 +1,80 @@
+var fotouploader_status_flg = true;
 $(document).ready(function(){
-  var $upload_modal = $('<div id="upload-modal" role="dialog"></div>');
-
-  var $upload_modal_content = $('<div id="upload-modal-content"></div>');
-  var $upload_message = $('<p></p>');
-
-  $upload_modal_content.append($upload_message);
-  $upload_modal.append($upload_modal_content);
+  "use strict";
+  var $upload_modal = $('<div id="fotouploader-modal-background" role="dialog"></div>');
   $upload_modal.on('click',function(){
-    $(this).fadeOut();
+    if(fotouploader_status_flg) {
+      $('#message-modal').fadeOut();
+      $(this).fadeOut();
+    }
   });
   $upload_modal.on('mousemove',function(){
-    $(this).fadeOut();
+    if(fotouploader_status_flg) {
+      $('#message-modal').fadeOut();
+      $(this).fadeOut();
+    }
+  });
+  $('body').append($upload_modal);
+
+  var $message_modal = $('<div id="message-modal" class="fotouploader-modal-content"></div>');
+  var $upload_message = $('<p class="fotouploader-message"></p>');
+  $message_modal.append($upload_message);
+  $upload_modal.append($message_modal);
+
+  var $scale_modal = $('<div id="scale-modal" class="fotouploader-modal-content"></div>');
+  var $scale_upload_input = $('<div><input id="fotouploader-scale" type="text" name="scale" placeholder="縮尺率を入力"></input>%</div>');
+  var $scale_upload_button = $('<button class="fotouploader-button" name="submit">アップロード</button>');
+  $scale_upload_button.on('click',function(){
+    var $scale_modal = $('#scale-modal');
+    var scale = $scale_modal.find('input[name="scale"]').val();
+    if (scale.match(/\D+/) || scale < 10 || scale > 200) {
+      // エラーメッセージが出力されている場合はそのまま
+      var $error_message = $scale_modal.find('.fotouploader-message-error');
+      if($error_message.length == 0) {
+        $error_message = $('<p class="fotouploader-message fotouploader-message-error">縮小率は10〜200の範囲で入力して下さい</p>');
+        $('#scale-modal').prepend($error_message);
+      }
+    } else {
+      $scale_modal.fadeOut();
+      chrome.runtime.sendMessage({scale: scale,src: $('#fotouploader-src').val()});
+    }
+  });
+  var $scale_cancel_button = $('<button class="fotouploader-button" name="cancel">キャンセル</button>');
+  $scale_cancel_button.on('click',function() {
+    $('#fotouploader-modal-background').fadeOut();
   });
 
-  $('body').append($upload_modal);
+  $scale_modal.append($('<hidden id="fotouploader-src"></hidden>'));
+  $scale_modal.append($scale_upload_input);
+  $scale_modal.append($('<div class="scale-buttons"/>').append($scale_upload_button));
+  $scale_modal.append($('<div class="scale-buttons"/>').append($scale_cancel_button));
+  $upload_modal.append($scale_modal);
+
 
   chrome.runtime.onMessage.addListener(function(message) {
     $(this).blur();
-    var json_message = JSON.parse(message);
-    var $upload_modal = $('#upload-modal');
-    var $upload_modal_content = $('#upload-modal-content');
-    $upload_modal_content.find('p').text(json_message['message']);
+    $('#fotouploader-modal-background').fadeIn();
 
-    $('#upload-modal').fadeIn();
+    var json_message = JSON.parse(message);
+    var message_type = json_message['type'];
+    if (message_type == "notice") {
+      fotouploader_status_flg = true;
+      var $message_modal = $('#message-modal');
+      $message_modal.find('p').text(json_message['message']);
+      $message_modal.fadeIn();
+    } else {
+      fotouploader_status_flg = false;
+      var $scale_modal = $('#scale-modal');
+
+      // エラーメッセージが出っぱなしの場合は削除する
+      var $error_message = $scale_modal.find('.fotouploader-message-error');
+      if ($error_message.length > 0) {
+        $error_message.remove();
+      }
+      $('#fotouploader-src').val(json_message['src']);
+      $('#fotouploader-scale').val('');
+      $upload_modal.fadeIn();
+      $scale_modal.fadeIn();
+    }
   });
 });
